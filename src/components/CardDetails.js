b5bcd65610e0839cb1ff0f158a84f4976340059e
AndersonCGAntunes <andersonclaytondesenvolvedor@gmail.com>
2024-01-07 17:38:17 -0300

    Alteração de detalhes da carta.

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -8,7 +8,7 @@ const CardDetails = ({ cardData }) => {
     <div className='eleDetails'>
       <h3>Detalhes da carta</h3>
       <p><strong>Elemental:</strong> {elemental}</p>
-      <p><strong>Pode Modificar em:</strong> {cardMod}</p>
+      <p><strong>Ao refinar a carta... </strong> {cardMod}</p>
       <p><strong>Como Obter:</strong></p>
       <ul>
         {howToObtain.map((element, index) => (
